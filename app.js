6af25eb0b3e55b4c8ba5e1229109b58b8322fae8
Sakaz22 <90087474+Sakaz22@users.noreply.github.com>
2022-05-12 08:01:35 +0200

    JavaScript Calculator

---
+++ app.js
@@ -0,0 +1,17 @@
+let result
+let fnumber
+let snumber
+let text = document.getElementById('app')
+let formbox = document.getElementById('form')
+let inputbox = document.getElementById('input').value
+let btnsubmit = document.getElementById('submit')
+switch (inputbox) {
+    case 1: function addition (){
+        fnumber = prompt('Please enter the first number')
+        snumber = prompt('Please enter the first number')
+        return result = fnumber + snumber;}
+        break;
+
+    default:
+        break;
+}
