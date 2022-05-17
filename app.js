98d4b1c985e8b7c44fd5b171b0c6c7d820ed53f8
Sakaz22 <90087474+Sakaz22@users.noreply.github.com>
2022-05-17 07:08:06 +0200

    Bootstrap and stripped some features off
    I added some bootstrap styling to make the calculator more appealing to use and to make it responsive.

--- app.js
+++ app.js
@@ -2,38 +2,41 @@ let result
 let fnumber
 let snumber
 let text = document.getElementById('app')
+
+
+let btn = document.getElementById('startFtn')
+btn.onclick = function startCalculator(){
 let popup = parseInt(prompt("Enter a Number"))
 switch (popup) {
-    case 1: function operation (){
+    case 1:
         fnumber = parseInt(prompt('Please enter the first number'))
         snumber = parseInt(prompt('Please enter the second number'))
         result = fnumber + snumber;
-        return result;
-        }
+        console.log(result)
         break;
     
-    case 2: function operation (){
+    case 2: 
         fnumber = parseInt(prompt('Please enter the first number'))
         snumber = parseInt(prompt('Please enter the second number'))
         result = fnumber - snumber;
-        }
+        console.log(result)
         break;
 
-    case 3: function operation (){
+    case 3: 
         fnumber = parseInt(prompt('Please enter the first number'))
         snumber = parseInt(prompt('Please enter the second number'))
         result = fnumber * snumber;
-        }
+        console.log(result)
         break;
 
-    case 4: function operation (){
+    case 4:
         fnumber = parseInt(prompt('Please enter the first number'))
         snumber = parseInt(prompt('Please enter the second number'))
         result = fnumber / snumber;
-        }
+        console.log(result)
         break;
+        
     default:
         break;
 }
-operation();
-console.log(result)
+}
