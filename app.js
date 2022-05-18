ffed61559c259fd313e93f426597b1b577f321fe
Sakaz22 <90087474+Sakaz22@users.noreply.github.com>
2022-05-18 07:12:09 +0200

    Creating the original calculator

--- app.js
+++ app.js
@@ -7,33 +7,36 @@ let text = document.getElementById('app')
 let btn = document.getElementById('startFtn')
 btn.onclick = function startCalculator(){
 let popup = parseInt(prompt("Enter a Number"))
+
+let output = document.getElementById("output")
+
 switch (popup) {
     case 1:
         fnumber = parseInt(prompt('Please enter the first number'))
         snumber = parseInt(prompt('Please enter the second number'))
         result = fnumber + snumber;
-        console.log(result)
+        output.textContent = result;
         break;
     
     case 2: 
         fnumber = parseInt(prompt('Please enter the first number'))
         snumber = parseInt(prompt('Please enter the second number'))
         result = fnumber - snumber;
-        console.log(result)
+        output.textContent = result;
         break;
 
     case 3: 
         fnumber = parseInt(prompt('Please enter the first number'))
         snumber = parseInt(prompt('Please enter the second number'))
         result = fnumber * snumber;
-        console.log(result)
+        output.textContent = result;
         break;
 
     case 4:
         fnumber = parseInt(prompt('Please enter the first number'))
         snumber = parseInt(prompt('Please enter the second number'))
         result = fnumber / snumber;
-        console.log(result)
+        output.textContent = result;
         break;
         
     default:
