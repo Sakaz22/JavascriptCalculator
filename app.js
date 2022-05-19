fe681ed12947194bb0bf69f041489caebd597e1e
Sakaz22 <90087474+Sakaz22@users.noreply.github.com>
2022-05-19 09:03:01 +0200

    Added some number variables for the calculator

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ let text = document.getElementById('app')
 
 
 let btn = document.getElementById('startFtn')
+
 btn.onclick = function startCalculator(){
 let popup = parseInt(prompt("Enter a Number"))
 
@@ -43,3 +44,25 @@ switch (popup) {
         break;
 }
 }
+
+// The numbers
+let one = document.getElementById("one");
+let two = document.getElementById("two");
+let three = document.getElementById("three");
+let four = document.getElementById("four");
+let five = document.getElementById("five");
+let six = document.getElementById("six");
+let seven = document.getElementById("seven");
+let eight = document.getElementById("eight");
+let nine = document.getElementById("nine");
+let zero = document.getElementById("zero");
+let dot = document.getElementById("dot");
+let add = document.getElementById("add");
+let subtract = document.getElementById("subtract");
+let divide = document.getElementById("divide");
+let multiply = document.getElementById("multiply");
+let equals = document.getElementById("equals");
+
+let numbers = [one,two,three,four,five,six,seven,eight,nine,zero]
+
+console.log(numbers)
